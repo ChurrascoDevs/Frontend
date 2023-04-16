9f8393718ffa2ca7597e31c7875300f978557d7c
jatorres <jatorres@alumnos.inf.utfsm.cl>
2023-04-16 17:51:43 -0400

    modificación importación css y  se añade vista 1 ejemplo

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './common/Header';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// importen aqui sus componentes
+import Header from './common/Header';
+import LibraryCatalog from './Vista 1/LibraryCatalog';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
